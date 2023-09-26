c2e9fe411980e0e8083384cd250c01aaed40e614
Jason Horsley <32709073+JasonHorsleyTech@users.noreply.github.com>
2023-09-25 22:32:13 -0500

    A resume :)

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -12,52 +12,17 @@ defineProps<{
 <template>
     <Head title="Welcome" />
 
-    <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
-    >
+    <div class="grid place-content-center bg-gray-100 min-h-screen">
         <div
-            v-if="canLogin"
-            class="sm:fixed sm:top-0 sm:right-0 p-6 text-right"
+            class="shadow-xl rounded-lg w-[36rem] min-h-[32rem] border bg-gray-200 p-4"
         >
-            <Link
-                v-if="$page.props.auth.user"
-                :href="route('dashboard')"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Dashboard</Link
+            <h2
+                class="text-3xl font-bold border-b pb-2 mb-2 border-gray-800/25"
             >
-
-            <!-- No real log-in required features... yet -->
-            <!-- <template v-else>
-                <Link
-                    :href="route('login')"
-                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Log in</Link
-                >
-
-                <Link
-                    v-if="canRegister"
-                    :href="route('register')"
-                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Register</Link
-                >
-            </template> -->
+                Jason Horsley
+            </h2>
+            <p>Full Stack Developer</p>
+            <p>I'm really good at fintech</p>
         </div>
-        <h2>Deploy test</h2>
     </div>
-
-    <!-- <div class="grid place-content-center">
-        <iframe
-            src="https://player.vimeo.com/video/868053959?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-            frameborder="0"
-            allow="autoplay; fullscreen; picture-in-picture"
-            style="
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-            "
-            title="2023-09-25 13-56-29"
-        ></iframe>
-    </div> -->
 </template>
