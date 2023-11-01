48c10c4b47b3fe73073fa5b1b60bca2d52f1417b
Jason Horsley <32709073+JasonHorsleyTech@users.noreply.github.com>
2023-11-01 16:18:58 -0500

    Added resume chooser and logic

--- resources/js/Singletons/Resume.vue
+++ resources/js/Singletons/Resume.vue
@@ -1,81 +1,120 @@
 <script lang="ts" setup>
 import axios from "axios";
+import "@/../scss/ResumeMarkdownItStyleOverrides.scss";
 import MarkdownIt from "markdown-it";
 import { ref } from "vue";
 import { onMounted } from "vue";
 import Loader from "@/Components/Loader.vue";
+import { computed } from "vue";
+import { watch } from "vue";
 
-const markdownContent = ref<string | null>(null);
-const htmlContent = ref<string | null>(null);
 const md = new MarkdownIt();
 
+const resume = ref<{
+    id: number;
+    name: string;
+    content: string;
+} | null>(null);
+const resumes = ref<{ id: number; name: string }[]>([]);
+const company_name = ref<string | null>(null);
+
+interface ResumeControllerIndexResponse {
+    data: {
+        resumes: { id: number; name: string }[];
+    };
+}
+
+interface ResumeControllerShowResponse {
+    data: {
+        id: number;
+        name: string;
+        content: string;
+    };
+}
+
+interface ResumeControllerInvokeResponse {
+    data: {
+        resume: {
+            id: number;
+            name: string;
+            content: string;
+        };
+        company_name: null | string;
+    };
+}
+
 onMounted(async () => {
-    const { data } = await axios.get("/api/professional/resume");
-    markdownContent.value = data.resume;
-    htmlContent.value = md.render(data.resume);
+    const resumeIndexResponse = (await axios.get(
+        "/api/professional/resumes"
+    )) as ResumeControllerIndexResponse;
+    resumes.value = resumeIndexResponse.data.resumes;
+
+    const { data } = (await axios.get(
+        "/api/professional/resume"
+    )) as ResumeControllerInvokeResponse;
+    resume.value = data.resume;
+    selectedResumeId.value = data.resume.id;
+
+    company_name.value = data.company_name;
 });
+
+const htmlContent = computed(() => {
+    if (resume.value === null) return null;
+
+    return md.render(resume.value.content);
+});
+
+const selectedResumeId = ref<number | null>(null);
+watch(selectedResumeId, (resume_id) => {
+    if (resume_id === null) return;
+    if (resume_id === resume.value?.id) return;
+
+    fetchResume(resume_id);
+});
+const fetchResume = async (resume_id: number) => {
+    resume.value = null;
+
+    const { data } = (await axios.get(
+        `/api/professional/resumes/${resume_id}`
+    )) as ResumeControllerShowResponse;
+
+    resume.value = data;
+};
 </script>
 
 <template>
-    <div
-        class="shadow-xl rounded-lg border bg-[#EDF2F7] dark:border-[#4B5563] dark:bg-[#EDF2F7]/20 p-4"
-    >
-        <Loader v-if="!htmlContent" />
+    <div class="grid gap-2">
         <div
-            v-else
-            class="prose dark:prose-invert min-h-[32rem]"
-            v-html="htmlContent"
-        ></div>
+            class="shadow-xl rounded-lg border bg-[#EDF2F7] dark:border-[#4B5563] dark:bg-[#EDF2F7]/20 p-4"
+        >
+            <Loader v-if="!htmlContent" />
+            <div
+                v-else
+                class="prose dark:prose-invert min-h-[32rem]"
+                v-html="htmlContent"
+            ></div>
+        </div>
+
+        <div class="ml-auto">
+            <div v-if="company_name">
+                <span>Pssst! You're {{ company_name }} right?</span>
+                <span>
+                    This is the resume I sent you, but I'll let you peak at the
+                    others too.
+                </span>
+            </div>
+            <select v-model="selectedResumeId">
+                <option value="null" disabled selected>
+                    Select a different resume
+                </option>
+                <option
+                    v-for="resume in resumes"
+                    :key="resume.id"
+                    :value="resume.id"
+                >
+                    {{ resume.name }}
+                </option>
+            </select>
+        </div>
     </div>
 </template>
-
-<style lang="scss">
-h1 {
-    img {
-        @apply h-12 w-12 m-0 #{!important};
-    }
-    @apply flex items-center gap-4 #{!important};
-}
-h2 {
-    @apply border-b border-gray-300 pb-4 #{!important};
-}
-
-h3 {
-    @apply border-t border-dashed border-gray-300 mt-4 pt-4 #{!important};
-}
-
-li::marker {
-    @apply text-[#3398DB] dark:text-white #{!important};
-}
-
-h3:first-of-type {
-    @apply border-t-0 mt-0 pt-0 #{!important};
-}
-
-blockquote {
-    @apply m-0 #{!important};
-}
-blockquote p {
-    @apply m-0 #{!important};
-}
-
-table {
-    @apply border bg-black/25 dark:bg-gray-500 shadow-lg rounded-lg overflow-hidden w-full #{!important};
-
-    thead {
-        @apply bg-gray-100 dark:bg-gray-700 #{!important};
-        tr {
-            @apply border-b-[2px] border-gray-400/50 dark:border-gray-500/50 #{!important};
-        }
-    }
-
-    tbody {
-        @apply bg-gray-100 dark:bg-gray-900 #{!important};
-    }
-
-    td,
-    th {
-        @apply border border-gray-300/50 dark:border-gray-700 px-4 py-2 #{!important};
-    }
-}
-</style>
