af4d5e76592d5247d755b71acd032058b7062b95
Jason Horsley <32709073+JasonHorsleyTech@users.noreply.github.com>
2024-01-18 12:56:48 -0600

    Test route for friend

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::post('/track/video/{id}', 'VideoController@create');
+// Route::post('/track/video/{id}', 'VideoController@create');``
 
 // Route group prefixed by "professional", first route is get on /resume which uses ResumeController at invote
 Route::prefix('professional')->group(function () {
@@ -30,3 +30,10 @@ Route::prefix('professional')->group(function () {
 });
 
 Route::get('/lyrics/{song}', [LyricController::class, '__invoke'])->name('lyrics.show');
+
+Route::post('/test', function (Request $request) {
+    return response()->json([
+        'message' => 'Hello World!',
+        'request' => $request->all(),
+    ]);
+});
